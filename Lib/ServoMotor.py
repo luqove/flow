e29b4b4b768f1a2cb3256dbb3a3488bca99ec674
chen <37654765+luqove@users.noreply.github.com>
2022-05-12 09:49:29 +1000

    Update ServoMotor.py

--- Lib/ServoMotor.py
+++ Lib/ServoMotor.py
@@ -4,21 +4,12 @@ import time
 
 class ServoMotor(object):
     """
-<<<<<<< HEAD
-   servo
+    servo
     """
 
-    # pin。
+    # pin
     def __init__(self, servo_pin):
-        # current angle
-=======
-    Servo
-    """
-
-    # pin就是对应的引脚。
-    def __init__(self, servo_pin):
-        # 记录目前的角度，可以根据这个判断门的开关。
->>>>>>> ba00b04ea178c39d71c452e25ef1d277abf379af
+        # angle
         self.angle = 0
         self.servo_pin = servo_pin
         self.gpio_setup()
@@ -31,13 +22,7 @@ class ServoMotor(object):
         self.pwm.start(0)
         self.pwm.ChangeDutyCycle(7.5)  # Set starting position
 
-<<<<<<< HEAD
-    # To achieve rotation, to achieve door opening
-    # or to rotate the belt。
-=======
-    # 实现转动，来达成开门，或者转动皮带
-    # 参数是输入的脉冲。
->>>>>>> ba00b04ea178c39d71c452e25ef1d277abf379af
+    # To achieve rotation, to achieve door opening, or to rotate the belt
     def act(self, angle):
         if 0 <= angle <= 180:
             duty = (angle / 18) + 2
